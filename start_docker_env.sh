851f6e0e9bb7cc5752c498991a89a25c90f099f8
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-07-04 21:58:39 +0200

    Use python3 to match the class material

--- start_docker_env.sh
+++ start_docker_env.sh
@@ -1,4 +1,4 @@
-docker run -d --name spark singularities/spark start-spark master
+docker run -d --name spark -e 'PYSPARK_PYTHON=python3' singularities/spark start-spark master
 
 echo "Started Spark container."
 echo " - To open a shell in the container, type \"docker exec -it spark bash\""
