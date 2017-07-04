3012e416af49184bd8dd329d5f3612e21d145d6d
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-07-04 22:25:45 +0200

    Add workaround for spark-docker image:
    https://github.com/SingularitiesCR/spark-docker/issues/4

---
+++ README.md
@@ -0,0 +1,5 @@
+The commands: 
+- rdd = sc.textFile("filename")
+- rdd.count()
+
+should return the line count of that file
--- start_docker_env.sh
+++
@@ -1,4 +0,0 @@
-docker run -d --name spark -e 'PYSPARK_PYTHON=python3' singularities/spark start-spark master
-
-echo "Started Spark container."
-echo " - To open a shell in the container, type \"docker exec -it spark bash\""
